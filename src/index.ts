cb7e2a3766a3b6cee3c1d9d5111ec420d2e0b0ba
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-07-11 14:51:23 +0900

    feat(bun): implement basic structure for CLI tool

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello via Bun!');
+import { parseArgs } from 'node:util';
+
+export default class Owl {
+  run = async () => {
+    const { positionals } = parseArgs({
+      args: Bun.argv.slice(2),
+      strict: true,
+      allowPositionals: true,
+    });
+
+    switch (positionals.at(0)) {
+      default:
+        console.log('Hello via Bun!');
+    }
+  };
+}
